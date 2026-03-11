1f7165d485a0af3cd49304b9efe5d67e5ca8c91d
Luke Andrew Shore <luke.shore@olivaenergy.nl>
2026-03-11 15:06:13 +0000

    logo gose home & when page changes go to top of page

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,7 @@ import {Care} from './pages/care/care';
 import {Systems} from './pages/systems/systems';
 import {Contact} from './pages/contact/contact';
 import {LanguageSwitcher} from './components/language-switcher/language-switcher';
+import {ViewportScroller} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +14,12 @@ import {LanguageSwitcher} from './components/language-switcher/language-switcher
 export class App
 {
   protected current_page_index: number = 0;
+
+  constructor(private viewportScroller: ViewportScroller) {}
+
   protected setPageIndex(index: number)
   {
     this.current_page_index = index;
+    this.viewportScroller.scrollToPosition([0, 0]);
   }
 }
