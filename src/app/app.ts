4dc2d630e46c4c1eafe3fcc2c1778a97408232e6
Luke Andrew Shore <luke.shore@olivaenergy.nl>
2026-03-13 16:20:17 +0000

    fix page history and refresh + home text alignment + meta data

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {Home} from './pages/home/home';
 import {Care} from './pages/care/care';
 import {Systems} from './pages/systems/systems';
@@ -11,15 +11,34 @@ import {NgOptimizedImage, ViewportScroller} from '@angular/common';
   imports: [Home, Care, Systems, Contact, LanguageSwitcher, NgOptimizedImage],
   templateUrl: './app.html'
 })
-export class App
+export class App implements OnInit
 {
   protected current_page_index: number = 0;
 
   constructor(private viewportScroller: ViewportScroller) {}
 
+  ngOnInit()
+  {
+    const saved = sessionStorage.getItem('pageIndex');
+    const pageIndex = saved ? parseInt(saved, 10) : 0;
+    this.current_page_index = pageIndex;
+    history.replaceState({ pageIndex }, '');
+  }
+
   protected setPageIndex(index: number)
   {
     this.current_page_index = index;
+    sessionStorage.setItem('pageIndex', String(index));
+    this.viewportScroller.scrollToPosition([0, 0]);
+    history.pushState({ pageIndex: index }, '');
+  }
+
+  @HostListener('window:popstate', ['$event'])
+  onPopState(event: PopStateEvent)
+  {
+    const pageIndex = event.state?.pageIndex ?? 0;
+    this.current_page_index = pageIndex;
+    sessionStorage.setItem('pageIndex', String(pageIndex));
     this.viewportScroller.scrollToPosition([0, 0]);
   }
 }
